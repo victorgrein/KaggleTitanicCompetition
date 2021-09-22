e603d66e7444b94be201bf781177a02627785427
Victor Henrique Grein <contatovhg@gmail.com>
2021-09-22 14:37:41 -0300

    Update My Titanic submission code.py

--- My Titanic submission code.py
+++ My Titanic submission code.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
 
 data = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -28,15 +21,6 @@ data = clean(data)
 test = clean(test)
 
 
-# In[3]:
-
-
-data.head(5)
-
-
-# In[4]:
-
-
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 
@@ -47,12 +31,6 @@ for col in cols:
     test[col] = le.transform(test[col])
     print(le.classes_)
 
-data.head(5)
-
-
-# In[5]:
-
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
@@ -60,42 +38,17 @@ y = data["Survived"]
 X = data.drop("Survived", axis=1)
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state = 42)
-
-
-# In[6]:
-
-
 clf = LogisticRegression(random_state=0, max_iter=1000).fit(X_train, y_train)
-
-
-# In[7]:
-
 
 predictions = clf.predict(X_val)
 from sklearn.metrics import accuracy_score
+
 accuracy_score(y_val, predictions)
-
-
-# In[8]:
-
-
 submission_preds = clf.predict(test)
-
-
-# In[9]:
-
 
 df = pd.DataFrame({"PassengerId":test_ids.values,
                    "Survived":submission_preds,})
-
-
-# In[10]:
-
-
 df.to_csv("submission.csv", index=False)
-
-
-# In[ ]:
 
 
 
